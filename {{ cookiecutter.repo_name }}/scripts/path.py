61975bfded7f08af6b6ee9f774f46b0fbd4595a8
flaviomorelli <38486294+flaviomorelli@users.noreply.github.com>
2022-07-08 14:52:56 +0200

    Moved .gitignore to the post generation script

--- {{ cookiecutter.repo_name }}/scripts/path.py
+++
@@ -1,9 +0,0 @@
-from importlib.resources import path
-import pathlib
-
-HERE = pathlib.Path(".").absolute()
-REPO_ROOT = HERE.parent()
-data = REPO_ROOT / "data"
-results = REPO_ROOT / "results"
-checkpoints = results / "models"
-figures = results / "figures"
